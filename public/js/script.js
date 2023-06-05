509413ab1823b544c75b648dc478736a859ef577
Guilherme Diorio <guilherme@rxredefined.com>
2023-06-05 07:35:15 -0300

    Refactor: in logic of toggle function

--- public/js/script.js
+++ public/js/script.js
@@ -13,18 +13,10 @@ const darkColor = "#212121";
 
 toggleDark = () => {
     toggle(toggleIcon, "bi-sun-fill", "bi-moon-fill", 1, darkColor, lightColor);
-
-    // toggleLogo.style.filter = "invert(1)";
-    // document.documentElement.style.setProperty("--primary-color", darkColor);
-    // document.documentElement.style.setProperty("--secondary-color", lightColor);
 };
 
 toggleLight = () => {
     toggle(toggleIcon, "bi-moon-fill", "bi-sun-fill", 0, lightColor, darkColor);
-
-    // toggleLogo.style.filter = "invert(0)";
-    // document.documentElement.style.setProperty("--primary-color", lightColor);
-    // document.documentElement.style.setProperty("--secondary-color", darkColor);
 };
 
 const toggle = (
