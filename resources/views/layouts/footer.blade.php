573f469a0c61cb5909c36492b0eb25dbf4dd4483
Guilherme Diorio <guilherme@rxredefined.com>
2023-06-05 15:08:20 -0300

    WIP: footer stylization

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,96 @@
-<footer>
+<footer class="footer-color">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col">
+
+                <h5 class="font-weight-700">Página inicial</h5>
+
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Cadastrar-se</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Netus fermentum</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Suspendisse viverra</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Id dolor</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Erat mattis</a>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div class="col">
+                <ul class="nav flex-column">
+                    <h5>Informações</h5>
+
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Termos de uso</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Sobre nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Como o site funciona?</a>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="col">
+                <ul class="nav flex-column">
+                    <h5>Minha conta</h5>
+
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Sair</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Área do usuário</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Configurações</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-0" href="">Privacidade</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="footer-form" class="col-4 py-5 px-5">
+
+                <form>
+                    <label for="input-email" class="form-label pb-3">
+                        Subscribe
+                    </label>
+                    <div id="form-control" class="d-flex f-row">
+                        <input type="email" class="form-control" id="input-email">
+                        <button type="submit" class="btn">sb</button>
+                    </div>
+
+                    <p class="pt-3" id="form-typography">
+                        Gravida sed justo, justo, id est et. Amet tristique convallis
+                        sed
+                        porttitor
+                        nullam eu
+                        ut. Duis et
+                        odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi.
+                    </p>
+                </form>
+
+
+            </div>
+        </div>
+    </div>
+    <div class="row">
+
+    </div>
+    </div>
 </footer>
 <script defer src="{{ asset('js/script.js') }}"></script>
 </body>
