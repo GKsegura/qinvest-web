169f46120472df601949ff8b5f2f846370c1f0ff
Guilherme Diorio <guilherme@rxredefined.com>
2023-06-13 10:11:11 -0300

    New: box shadow in header

--- resources/views/layouts/wave.blade.php
+++ resources/views/layouts/wave.blade.php
@@ -1,5 +1,5 @@
 <svg width="1425" height="737" viewBox="0 0 1425 737" fill="none" xmlns="http://www.w3.org/2000/svg"
-    class="z-0 position-absolute w-100 h-100">
+    class="position-absolute w-100 h-100">
     <path fill-rule="evenodd" clip-rule="evenodd"
         d="M-15 737C-15 737 293.277 625.263 493.292 621.899C693.306 618.535 797.106 709.304 972.909 686.284C1148.71 663.263 1426 533.038 1426 533.038V-100H-15V737Z"
         fill="url(#paint0_linear_4_1903)" />
