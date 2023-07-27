96d8747b2dd076d839db0562418d8488133bcc2d
Deoola <91548957+Deoola@users.noreply.github.com>
2023-07-27 19:45:42 -0300

    new: password encryption and English translation of files

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <!-- resources/views/cadastro.blade.php -->
-<form action="{{ route('cadastro') }}" method="POST">
+<form action="{{ route('register') }}" method="POST">
     @csrf
     <label for="nome">Nome:</label>
     <input type="text" name="nome" required>
@@ -8,7 +8,7 @@
     <input type="email" name="email" required>
     <br>
     <label for="senha">Senha:</label>
-    <input type="senha" name="senha" required>
+    <input type="password" name="senha" required>
     <br>
     <!-- Adicione outros campos do cadastro, se houver -->
     <button type="submit">Cadastrar</button>
