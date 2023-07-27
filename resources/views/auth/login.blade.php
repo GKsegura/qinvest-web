69305fd073a3625912af780ee34ba29564bdf326
José Segura <jose.segura@unesp.br>
2023-07-26 22:30:37 -0300

    WIP: login front

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,19 @@
 <div class="form-page">
     <div class="container">
         <div class="row">
-            <div class="side-left col xs-3">alo</div>
-            <div class="side-right col xs-3">cambio</div>
+            <div class="side-left col xs-2">
+                <div class="into-side-left col xs-2">
+                    <div class="square-form">
+                        <div class="text-left-form">o próximo passo na sua <span class="highlighted">jornada</span>
+                        </div>
+                        <div class="bottom-left-form">
+                            <div class="text-bottom-left">Faça o seu cadastro na QINVEST◢ e participe dessa
+                                comunidade</div><img src="assets/form-stair.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="side-right col xs-2"></div>
         </div>
     </div>
 </div>
