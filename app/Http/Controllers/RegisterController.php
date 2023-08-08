a32a726e33f28e13038117d96a3a24a4dba18fa1
Deoola <91548957+Deoola@users.noreply.github.com>
2023-08-08 15:19:21 -0300

    WIP: Register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+$timestamp = Carbon::now();
 
 class RegisterController extends Controller
 {
@@ -18,17 +21,28 @@ class RegisterController extends Controller
     {
         // Valide os dados recebidos
         $request->validate([
-            'nome' => 'required|string|max:150',
-            'email' => 'required|string|max:150',
-            'senha' => 'required|string|max:200',
+            'username' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+            'genero' => 'required|string|max:50',
+            'create_time' => 'required|timestamp|max:255',
+            'newsletter' => 'required|boolean',
+            'terms_user' => 'required|boolean',
+            'birth_time' => 'required|date',
+            'deleted' => 'required|boolean'
         ]);
 
         // Execute o INSERT na tabela de usuários
-        DB::table('usuario')->insert([
-            'nome' => $request->input('nome'),
+        DB::table('User')->insert([
+            'username' => $request->input('username'),
             'email' => $request->input('email'),
-            'senha' => Hash::make($request->input('senha')), // Criptografa a senha
-            'excluido' => false,
+            'password' => Hash::make($request->input('password')), // Criptografa a senha
+            'genero' => $request->input('genero'),
+            'create_time' => Carbon::now('America/Sao_Paulo'),
+            'newsletter' => $request->input('newsletter'),
+            'birth_time' => $request->input('birth_time'),
+            'terms_user' => $request->input('terms_user'),
+            'deleted' => false
         ]);
 
         // Redirecione para uma página de sucesso ou exiba uma mensagem
--- resources/views/auth/form/register.blade.php
+++ resources/views/auth/form/register.blade.php
@@ -6,31 +6,42 @@
 
     <div class="field">
         <label for="nome" class="label-form">Nome</label>
-        <input for="nome" class="form-control" type="text" name="nome" required>
+        <input id="nome" class="form-control" type="text" name="username" required>
     </div>
 
     <div class="field">
         <label for="email" class="label-form">Email</label>
-        <input for="email" class="form-control" type="email" name="email" required>
+        <input id="email" class="form-control" type="email" name="email" required>
+    </div>
+
+    <div class="field">
+        <label for="genero" class="label-form">Gênero</label>
+        <input id="genero" class="form-control" type="text" name="genero" required>
+    </div>
+
+    <div class="field">
+        <label for="birth_time" class="label-form">Data de Nascimento</label>
+        <input id="birth_time" class="form-control" type="date" name="birth_time" required>
     </div>
 
     <div class="field">
         <label for="password" class="label-form">Senha</label>
-        <input for="password" class="form-control" type="password" name="senha" id="password"
-            onkeyup="passwordValidator()" required>
-        <i id="eye-icon-password" class="bi bi-eye"></i>
-        <div class="password-strength-bar">
-            <span class="one"></span>
-            <span class="two"></span>
-            <span class="three"></span>
-        </div>
-        <div class="text-password"></div>
+        <input id="password" class="form-control" type="password" name="password" required>
     </div>
+
     <div class="field">
         <label for="confirm-password" class="label-form">Confirme a senha</label>
-        <input for="confirm-password" class="form-control" type="password" name="confirmacao_senha"
-            id="confirm-password" required>
-        <i id="eye-icon-confirm-password" class="bi bi-eye"></i>
+        <input id="confirm-password" class="form-control" type="password" name="password_confirmation" required>
+    </div>
+
+    <div class="field">
+        <label for="newsletter" class="label-form">Assinatura de Newsletter</label>
+        <input id="newsletter" type="radio" name="newsletter" value="1" required>
+    </div>
+
+    <div class="field">
+        <label for="terms_user" class="label-form">Aceite dos Termos de Uso</label>
+        <input id="terms_user"  type="radio" name="terms_user" value="1" required>
     </div>
 
     <!-- Adicione outros campos do cadastro, se houver -->
