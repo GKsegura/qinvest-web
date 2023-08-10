7ba4800948ec463a4c4a1ad3612cafa4ecee21bb
Deoola <91548957+Deoola@users.noreply.github.com>
2023-08-10 10:45:14 -0300

    WIP: register backend

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 
+
 $timestamp = Carbon::now();
 
 class RegisterController extends Controller
@@ -20,30 +21,34 @@ class RegisterController extends Controller
     public function auth(Request $request)
     {
         // Valide os dados recebidos
-        $request->validate([
-            'username' => 'required|string|max:255',
-            'email' => 'required|string|max:255',
-            'password' => 'required|string|max:255',
-            'genero' => 'required|string|max:50',
-            'create_time' => 'required|timestamp|max:255',
-            'newsletter' => 'required|boolean',
-            'terms_user' => 'required|boolean',
-            'birth_time' => 'required|date',
-            'deleted' => 'required|boolean'
-        ]);
+        try {
+            $request->validate([
+                'username' => 'required|string|max:255',
+                'email' => 'required|string|max:255',
+                'password' => 'required|string|max:255',
+                'gender' => 'required|string|max:50',
+                'birth_time' => 'required|date|',
+            ]);
+        } catch (\Exception $v) {
+            echo "<script type = 'text/javascript'>alert ('Erro na validação')</script>";
+        } 
 
         // Execute o INSERT na tabela de usuários
-        DB::table('User')->insert([
-            'username' => $request->input('username'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')), // Criptografa a senha
-            'genero' => $request->input('genero'),
-            'create_time' => Carbon::now('America/Sao_Paulo'),
-            'newsletter' => $request->input('newsletter'),
-            'birth_time' => $request->input('birth_time'),
-            'terms_user' => $request->input('terms_user'),
-            'deleted' => false
-        ]);
+        try {
+            DB::table('users')->insert([
+                'email' => $request->input('email'),
+                'username' => $request->input('username'),
+                'password' => Hash::make($request->input('password')),
+                'gender' => $request->input('gender'),
+                'newsletter' => $request->input('newsletter'),
+                'terms_user' => true,
+                'birth_time' => $request->input('birth_time'),
+                'deleted' => false,
+                'created_at' => Carbon::now('America/Sao_Paulo')
+            ]);
+        } catch (\Exception $e) {
+            echo "<script type = 'text/javascript'>alert ('Sei lá')</script>";
+        }
 
         // Redirecione para uma página de sucesso ou exiba uma mensagem
         //return redirect()->route('sucesso');
--- resources/views/auth/form/register.blade.php
+++ resources/views/auth/form/register.blade.php
@@ -14,10 +14,14 @@
         <input id="email" class="form-control" type="email" name="email" required>
     </div>
 
-    <div class="field">
+    <!--<div class="field">-->
         <label for="genero" class="label-form">Gênero</label>
-        <input id="genero" class="form-control" type="text" name="genero" required>
-    </div>
+        <select name="gender" id="gender">
+        <option value="male">Masculino</option>
+        <option value="female">Feminino</option>
+        <option value="other">Outro</option>
+    </select>
+    <!--</div>-->
 
     <div class="field">
         <label for="birth_time" class="label-form">Data de Nascimento</label>
@@ -36,14 +40,10 @@
 
     <div class="field">
         <label for="newsletter" class="label-form">Assinatura de Newsletter</label>
-        <input id="newsletter" type="radio" name="newsletter" value="1" required>
-    </div>
-
-    <div class="field">
-        <label for="terms_user" class="label-form">Aceite dos Termos de Uso</label>
-        <input id="terms_user"  type="radio" name="terms_user" value="1" required>
+        <input id="newsletter" type="checkbox" name="newsletter">
     </div>
 
     <!-- Adicione outros campos do cadastro, se houver -->
     <button type="submit" class="button-submit">Cadastrar</button>
 </form>
+
