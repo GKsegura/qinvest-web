b442d213c9937126ffb07a8f86857a9e59378155
Guilherme Diorio <guilherme@rxredefined.com>
2023-08-10 13:16:11 -0300

    NEW: login input validations

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,21 +8,21 @@ use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
-    // Mostra o formulário de login
+  
     public function showLoginForm()
     {
         return view('auth.page.login');
     }
 
-    // Processa o login
+/**
+ * 
+ */
     public function login(LoginRequest $request)
     {
         $credentials = $request->validated(); 
-
+        
 
         if (Auth::attempt($credentials)) {
-            //Autenticação bem-sucedida
-
             return redirect()->route('home'); // Redireciona para a página inicial após o login
         } else {
             return back()->withErrors([
