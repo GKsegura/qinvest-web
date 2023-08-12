cdb62deb0c25d5298932c53f65c8f5112f5b84d7
Guilherme Diorio <guilherme@rxredefined.com>
2023-08-12 09:21:35 -0300

    WIP: mult-step form

--- resources/views/auth/form/register.blade.php
+++ resources/views/auth/form/register.blade.php
@@ -1,4 +1,6 @@
 <!-- resources/views/auth/form/register.blade.php -->
+@vite(['resources/utils/alpine.js'])
+
 
 
 <form action="{{ route('register') }}" method="POST">
@@ -6,47 +8,76 @@
 
 
 
-    <div class="field">
-        <label for="email" class="label-form">Email</label>
-        <input id="email" class="form-control" type="email" name="email">
+    <div x-data="{ step: 1 }">
+
+        <div x-show="step === 1" class="form">
+            <div class="field">
+                <label for="email" class="label-form">Email</label>
+                <input id="email" class="form-control" type="email" name="email">
+            </div>
+
+
+            <div class="field">
+                <label for="password" class="label-form">Senha</label>
+                <input for="password" class="form-control" type="password" name="senha" id="password"
+                    onkeyup="passwordValidator()">
+                <i id="eye-icon-password" class="bi bi-eye"></i>
+                <div class="password-strength-bar">
+                    <span class="one"></span>
+                    <span class="two"></span>
+                    <span class="three"></span>
+                </div>
+                <div class="text-password"></div>
+            </div>
+
+            <div class="field">
+                <label for="confirm-password" class="label-form">Confirme a senha</label>
+                <input for="confirm-password" class="form-control" type="password" name="confirmacao_senha"
+                    id="confirm-password">
+                <i id="eye-icon-confirm-password" class="bi bi-eye"></i>
+            </div>
+
+            <button class="btn" @click.prevent="step = 2">-></button>
+
+        </div>
+
+        <div x-show="step === 2" class="form">
+
+            <div class="field">
+                <label for="genero" class="label-form">Gênero</label>
+                <select name="gender" class="form-control" id="gender">
+                    <option value="male">Masculino</option>
+                    <option value="female">Feminino</option>
+                    <option value="other">Outro</option>
+                </select>
+            </div>
+
+            <div class="field">
+                <label for="birth_time" class="label-form">Data de Nascimento</label>
+                <input id="birth_time" class="form-control" type="date" name="birth_time">
+            </div>
+
+            <div class="field">
+                <label for="nome" class="label-form">Nome</label>
+                <input id="nome" class="form-control" type="text" name="username">
+            </div>
+
+            <div class="field">
+                <label for="newsletter" class="label-form">Assinatura de Newsletter</label>
+                <input id="newsletter" class="form-control" type="checkbox" name="newsletter">
+            </div>
+
+            <div>
+                <button class="btn" @click.prevent="step = 1">
+                    <- </button>
+                        <button type="submit" class="btn">Cadastrar</button>
+            </div>
+
+        </div>
+
     </div>
 
-    <!--<div class="field">-->
-
-
-    <div class="field">
-        <label for="password" class="label-form">Senha</label>
-        <input id="password" class="form-control" type="password" name="password">
-    </div>
-
-    <div class="field">
-        <label for="confirm-password" class="label-form">Confirme a senha</label>
-        <input id="confirm-password" class="form-control" type="password" name="password_confirmation">
-
-    </div>
-
-    <!-- <label for="genero" class="label-form">Gênero</label>
-        <select name="gender" id="gender">
-        <option value="male">Masculino</option>
-        <option value="female">Feminino</option>
-        <option value="other">Outro</option>
-    </select>
-
-    <div class="field">
-        <label for="birth_time" class="label-form">Data de Nascimento</label>
-        <input id="birth_time" class="form-control" type="date" name="birth_time" >
-    </div>
-
-    <div class="field">
-        <label for="nome" class="label-form">Nome</label>
-        <input id="nome" class="form-control" type="text" name="username" >
-    </div>
-
-    <div class="field">
-        <label for="newsletter" class="label-form">Assinatura de Newsletter</label>
-        <input id="newsletter" type="checkbox" name="newsletter">
-    </div> -->
 
     <!-- Adicione outros campos do cadastro, se houver -->
-    <button type="submit" class="button-submit">Cadastrar</button>
+    <!-- <!-- <button type="submit" class="button-submit">Cadastrar</button> -->
 </form>
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 "resources/sass/app.scss",
                 "resources/js/app.js",
                 "resources/js/theme.js",
+                "resources/utils/alpine.js",
             ],
             refresh: true,
         }),
