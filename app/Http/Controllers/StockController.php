bd911149fe00de98078fd418566900c5aaf76b34
José Segura <jose.segura@unesp.br>
2023-08-14 10:44:29 -0300

    WIP: api

---
+++ app/Http/Controllers/StockController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class StockController extends Controller
+{
+    public function getStockQuotes(Request $request, $tickers)
+    {
+        $range = $request->input('range', '1mo');
+        $interval = $request->input('interval', '1d');
+        $fundamental = $request->input('fundamental', false);
+        $dividends = $request->input('dividends', false);
+
+        $response = Http::get("https://brapi.dev/api/quote/$tickers", [
+            'range' => $range,
+            'interval' => $interval,
+            'fundamental' => $fundamental,
+            'dividends' => $dividends,
+        ]);
+
+        $data = $response->json();
+
+        return response()->json($data);
+    }
+}
