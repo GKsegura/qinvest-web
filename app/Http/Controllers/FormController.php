42ff0c7d13f89fb58571ed79c7a1713ea5750534
Deoola <91548957+Deoola@users.noreply.github.com>
2023-08-28 10:08:11 -0300

    WIP: questionary

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -9,9 +9,14 @@ class FormController extends Controller
 {
     public function index()
     {
-        $questions = Question::all();
+        $question1 = Question::find(1);
+        $question2 = Question::find(2);
+        $question3 = Question::find(3);
+        $question4 = Question::find(5);
+        $question5 = Question::find(4);
+        $question6 = Question::find(6);
         $answers = Answer::all();
 
-        return view('auth.page.viewformulary', compact('questions', 'answers'));
-    }
+        return view('auth.page.viewformulary', compact('question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'answers'));
+   }
 }
--- resources/views/auth/form/viewformulary.blade.php
+++ resources/views/auth/form/viewformulary.blade.php
@@ -4,16 +4,71 @@
     
     <fieldset>
         <legend>Perguntas</legend>
-        @foreach($questions->where('id', 1) as $question )
+        <label>{{ $question1->text_question }}</label><br>
+    <br>
+    </fieldset>
+    <fieldset>
+        @foreach($answers->where('question_id', 1) as $answer)
             <div>
-                <label>{{ $question->text_question }}</label><br>
-                {{ $question->observation }}
+                <label>
+                    <input type="radio" name="selected_answer" value="{{ $answer->id }}">
+                    {{ $answer->text_answer }}
+                </label><br>
             </div>
         @endforeach
     </fieldset>
     <br>
+    <label>{{ $question2->text_question }}</label><br>
     <fieldset>
-        @foreach($answers->where('question_id', 1) as $answer)
+        @foreach($answers->where('question_id', 2) as $answer)
+            <div>
+                <label>
+                    <input type="radio" name="selected_answer" value="{{ $answer->id }}">
+                    {{ $answer->text_answer }}
+                </label><br>
+            </div>
+        @endforeach
+    </fieldset>
+    <br>
+    <label>{{ $question3->text_question }}</label><br>
+    <fieldset>
+        @foreach($answers->where('question_id', 3) as $answer)
+            <div>
+                <label>
+                    <input type="radio" name="selected_answer" value="{{ $answer->id }}">
+                    {{ $answer->text_answer }}
+                </label><br>
+            </div>
+        @endforeach
+    </fieldset>
+    <br>
+    <label>{{ $question4->text_question }}</label><br>
+    <fieldset>
+        @foreach($answers->where('question_id', 5) as $answer)
+            <div>
+                <label>
+                    <input type="radio" name="selected_answer" value="{{ $answer->id }}">
+                    {{ $answer->text_answer }}
+                </label><br>
+            </div>
+        @endforeach
+    </fieldset>
+    <br>
+    <label>{{ $question5->text_question }}</label><br>
+    <fieldset>
+        @foreach($answers->where('question_id', 4) as $answer)
+            <div>
+                <label>
+                    <input type="radio" name="selected_answer" value="{{ $answer->id }}">
+                    {{ $answer->text_answer }}
+                </label><br>
+            </div>
+        @endforeach
+    </fieldset>
+    <br>
+    <label>{{ $question6->text_question }}</label><br>
+    <fieldset>
+        @foreach($answers->where('question_id', 6) as $answer)
             <div>
                 <label>
                     <input type="radio" name="selected_answer" value="{{ $answer->id }}">
