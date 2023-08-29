95781c83c3d379f7167e72563c986aa07c1252a6
José Segura <jose.segura@unesp.br>
2023-08-29 11:58:31 -0300

    WIP: stock.css and api.js

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,29 +1,35 @@
 import "chartjs-adapter-moment";
 import { Chart, registerables } from "chart.js";
-import zoomPlugin from "chartjs-plugin-zoom";
-Chart.register(...registerables, zoomPlugin);
+Chart.register(...registerables);
 import { getThemeFromCookie } from "../utils/cookies";
 
-function calculateProfitPercentage(previousClose, currentClose) {
-    return ((currentClose - previousClose) / previousClose) * 100;
-}
+const stockDiv = document.getElementById("stockDiv");
 
-function calculateProfitPercentages(closePrices) {
-    const profitPercentages = [];
+document.addEventListener("DOMContentLoaded", () => {
+    const stockForm = document.getElementById("stockForm");
 
-    for (let i = 0; i < closePrices.length; i++) {
-        if (i === 0) {
-            profitPercentages.push(null);
-        } else {
-            const profitPercentage = calculateProfitPercentage(
-                closePrices[i - 1],
-                closePrices[i]
-            );
-            profitPercentages.push(profitPercentage);
-        }
+    stockForm.addEventListener("submit", handleFormSubmit);
+
+    async function handleFormSubmit(event) {
+        stockDiv.style.visibility = "visible";
+        event.preventDefault();
+
+        const tickers = document.getElementById("tickers").value;
+        const period = document.getElementById("period").value;
+
+        const data = await fetchStockData(tickers, period);
+        displayStockData(data);
     }
-    return profitPercentages;
-}
+
+    async function fetchStockData(tickers, period) {
+        const response = await fetch(`/api/stock/${tickers}/${period}`);
+        return await response.json();
+    }
+
+    function displayStockData(data) {
+        createCharts(data);
+    }
+});
 
 const createCharts = (data) => {
     const theme = getThemeFromCookie();
@@ -35,20 +41,14 @@ const createCharts = (data) => {
     const historicalData = data.results[0].historicalDataPrice;
     const dates = historicalData.map((stock) => new Date(stock.date * 1000));
     const closePrices = historicalData.map((stock) => stock.close);
-    const profitPercentages = calculateProfitPercentages(closePrices);
 
     const ctxStock = document.getElementById("stockChart").getContext("2d");
-    const ctxProfit = document.getElementById("profitChart").getContext("2d");
 
     const existingChartStock = Chart.getChart(ctxStock);
     if (existingChartStock) {
         existingChartStock.destroy();
     }
 
-    const existingChartProfit = Chart.getChart(ctxProfit);
-    if (existingChartProfit) {
-        existingChartProfit.destroy();
-    }
     function findMovingAverage(closePrices, start, end, period) {
         let sum = 0;
         const actualPeriod = Math.min(period, end - start + 1);
@@ -75,11 +75,10 @@ const createCharts = (data) => {
     const movingAveragePriceDataset = {
         label: "Moving Average Price",
         data: movingAverageDatasets,
-        borderColor: "rgba(255, 165, 0, 1)",
+        borderColor: "rgba(73, 255, 0, 1)",
         borderWidth: 1,
         fill: false,
         pointRadius: 0,
-        hidden: true,
     };
     const inter = 10;
     const intervals = Math.floor(dates.length / inter);
@@ -155,9 +154,9 @@ const createCharts = (data) => {
     ];
     allDonchianDatasets.forEach((dataset) => {
         if (dataset.range.label == rangL) {
-            dataset.backgroundColor = "rgba(153, 51, 100, 0.2)";
-            dataset.borderColor = "transparent";
-            dataset.borderWidth = 0;
+            dataset.backgroundColor = "rgba(121, 0, 255, 0.2)";
+            dataset.borderColor = "rgba(121, 0, 255, 1)";
+            dataset.borderWidth = 0.5;
             dataset.fill = 1;
             dataset.hidden = false;
             dataset.pointRadius = 0;
@@ -171,7 +170,7 @@ const createCharts = (data) => {
     const priceDataset = {
         label: "Price",
         data: closePrices,
-        backgroundColor: "rgba(144,0,255, 0.2)",
+        backgroundColor: "rgba(121, 0, 255, 0.2)",
         borderColor: "rgba(188, 102, 255, 1)",
         borderWidth: 1,
         labels: dates,
@@ -231,48 +230,7 @@ const createCharts = (data) => {
                         filter: (item) => item.text !== "",
                     },
                 },
-                zoom: {
-                    zoom: {
-                        wheel: {
-                            enabled: true,
-                        },
-                        pinch: {
-                            enabled: true,
-                        },
-                        mode: "x",
-                    },
-
-                    pan: {
-                        enabled: true,
-                        mode: "x", // Enable panning in both directions
-                    },
-                },
             },
         },
     });
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-    const stockForm = document.getElementById("stockForm");
-
-    stockForm.addEventListener("submit", handleFormSubmit);
-
-    async function handleFormSubmit(event) {
-        event.preventDefault();
-
-        const tickers = document.getElementById("tickers").value;
-        const period = document.getElementById("period").value;
-
-        const data = await fetchStockData(tickers, period);
-        displayStockData(data);
-    }
-
-    async function fetchStockData(tickers, period) {
-        const response = await fetch(`/api/stock/${tickers}/${period}`);
-        return await response.json();
-    }
-
-    function displayStockData(data) {
-        createCharts(data);
-    }
-});
--- resources/views/pages/stock.blade.php
+++ resources/views/pages/stock.blade.php
@@ -7,27 +7,17 @@
 
         <label class="label" for="period">Period:</label>
         <select id="period">
-        <option value="3mo">3 meses</option>
-        <option value="6mo">6 meses</option>
-        <option value="1mo">1 mês</option>
-        <option value="1y">1 ano</option>
-        <option value="5y">5 anos</option>
-    </select>
+            <option value="3mo">3 meses</option>
+            <option value="6mo">6 meses</option>
+            <option value="1mo">1 mês</option>
+            <option value="1y">1 ano</option>
+            <option value="5y">5 anos</option>
+        </select>
 
         <button type="submit" class="button">Fetch Data</button>
     </form>
-
-
-    <div id="stockData">
-        <ul id="stockList"></ul>
-    </div>
-    <div>
+    <div id="stockDiv">
         <canvas id="stockChart" class="chart-canvas"></canvas>
-    </div>
-
-    <div>
-        <canvas id="profitChart" class="chart-canvas"></canvas>
-
     </div>
 </div>
 @include('layouts.footer')
