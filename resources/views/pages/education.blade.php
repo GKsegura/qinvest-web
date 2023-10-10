8aeea25e6314818bc33043a13e41077fc390337c
Isaac Levi <isaac.levi@unesp.br>
2023-10-10 12:12:49 -0300

    changes: education.blade and wavebg.svg

--- resources/views/pages/education.blade.php
+++ resources/views/pages/education.blade.php
@@ -1,11 +1,22 @@
 <x-layout.app>
+<svg class="wave-svg" viewBox="0 0 1425 737" preserveAspectRatio="none" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path id="wave-path" fill-rule="evenodd" clip-rule="evenodd" d="M-15 737C-15 737 293.277 
+    625.263 493.292 621.899C693.306 618.535 797.106 709.304 972.909 686.284C1148.71 
+    663.263 1426 533.038 1426 533.038V-100H-15V737Z" fill="url(#paint0_linear_4_1903)"/>
+    <defs>
+    <linearGradient id="paint0_linear_4_1903" x1="705.5" y1="-100" x2="705.5" y2="737" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#DB33F7"/>
+    <stop offset="1" stop-color="#3860EC"/>
+    </linearGradient>
+    </defs>
+</svg>
     <div class="page-education">
         <div id="education-page">
+        <h1 id="title-education-page" class="text-education-page">Aprenda a Investir</h1>
             <div id="education-guide">
-            <h1 id="title-education-page" class="text-education-page">Aprenda a Investir</h1>
             <div class="education-summary">
                 <div class="left-summary">
-                    <img src="assets/education.svg" alt="">
+                    <img src="assets/education2.svg" alt="">
                 </div>
                 <div class="right-summary">
                     <svg id="qinvestLogoSm" viewBox="0 0 199 38" xmlns="http://www.w3.org/2000/svg">
