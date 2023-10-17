6dca97e412442a24a68b52e57612bb614070b8b7
José Antonio Segura Marques da Silva <ra2157043@CTIBAURU.FEB.UNESP.BR>
2023-10-17 12:09:03 -0300

    wip: chart in admin page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,14 +18,13 @@ class AdminController extends Controller
 {
     public function Statistics()
     {
-        if (Auth::check() && Auth::user()-> email == 'admin@qinvest.com')
-        {
+        if (Auth::check() && Auth::user()->email == 'admin@qinvest.com') {
             $testC = Test::where('deleted', false)->where('investor_id', 1);
             $testM = Test::where('deleted', false)->where('investor_id', 2);
             $testA = Test::where('deleted', false)->where('investor_id', 3);
 
             $activeTestIds = Test::where('deleted', false)->pluck('id')->toArray();
-            
+
             $female = User::where('gender', 'female')->where('deleted', false);
             $male = User::where('gender', 'male')->where('deleted', false);
             $other = User::where('gender', 'other')->where('deleted', false);
@@ -33,72 +32,60 @@ class AdminController extends Controller
             $users = User::where('deleted', false);
 
             $answersCounts = TestAnswer::selectRaw('answer_id, COUNT(*) as count')
-            ->whereIn('test_id', $activeTestIds)
-            ->whereIn('answer_id', [7, 8, 9, 10, 11])
-            ->groupBy('answer_id')
-            ->get();
+                ->whereIn('test_id', $activeTestIds)
+                ->whereIn('answer_id', [7, 8, 9, 10, 11])
+                ->groupBy('answer_id')
+                ->get();
 
             $answerA = 0;
             $answerB = 0;
             $answerC = 0;
             $answerD = 0;
             $answerE = 0;
-        
-            foreach ($answersCounts as $answerCount) 
-            {
-                if ($answerCount->answer_id == 7) 
-                {
+
+            foreach ($answersCounts as $answerCount) {
+                if ($answerCount->answer_id == 7) {
                     $answerA += $answerCount->count;
-                }
-                else if ($answerCount->answer_id == 8)
-                {
+                } else if ($answerCount->answer_id == 8) {
                     $answerB += $answerCount->count;
-                }
-                else if ($answerCount->answer_id == 9)
-                {
+                } else if ($answerCount->answer_id == 9) {
                     $answerC += $answerCount->count;
-                }
-                else if ($answerCount->answer_id == 10)
-                {
+                } else if ($answerCount->answer_id == 10) {
                     $answerD += $answerCount->count;
-                }
-                else if ($answerCount->answer_id == 11)
-                {
+                } else if ($answerCount->answer_id == 11) {
                     $answerE += $answerCount->count;
                 }
-            }    
+            }
 
             $answersTotal = $answerA + $answerB + $answerC + $answerD + $answerE;
 
-            $range1 = round(($answerA/$answersTotal)*100); 
-            $range2 = round(($answerB/$answersTotal)*100); 
-            $range3 = round(($answerC/$answersTotal)*100); 
-            $range4 = round(($answerD/$answersTotal)*100); 
-            $range5 = round(($answerE/$answersTotal)*100); 
+            $range1 = round(($answerA / $answersTotal) * 100);
+            $range2 = round(($answerB / $answersTotal) * 100);
+            $range3 = round(($answerC / $answersTotal) * 100);
+            $range4 = round(($answerD / $answersTotal) * 100);
+            $range5 = round(($answerE / $answersTotal) * 100);
 
             $countC = $testC->count();
             $countM = $testM->count();
             $countA = $testA->count();
             $test = $countA + $countC + $countM;
-            
+
             $countFem = $female->count();
             $countMale = $male->count();
             $countOther = $other->count();
             $users = $countFem + $countMale + $countOther;
 
-            $moderado = round(($countM/$test)*100);
-            $conservador = round(($countC/$test)*100);
-            $agressivo = round(($countA/$test)*100);
+            $moderado = round(($countM / $test) * 100);
+            $conservador = round(($countC / $test) * 100);
+            $agressivo = round(($countA / $test) * 100);
 
-            $women = round(($countFem/$users)*100);
-            $men = round(($countMale/$users)*100);
-            $NI = round(($countOther/$users)*100);
+            $women = round(($countFem / $users) * 100);
+            $men = round(($countMale / $users) * 100);
+            $NI = round(($countOther / $users) * 100);
 
-            return view('pages.admin', compact('conservador', 'moderado', 'agressivo', 'test', 'women', 'men', 'NI', 'range1', 'range2', 'range3', 'range4', 'range5', 'users'));   
-        } 
-        else 
-        {
+            return view('pages.admin', compact('conservador', 'moderado', 'agressivo', 'test', 'women', 'men', 'NI', 'range1', 'range2', 'range3', 'range4', 'range5', 'users'));
+        } else {
             return redirect()->route('index');
-        }   
+        }
     }
 }
--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -13,31 +13,136 @@
 
     <div class="page">
         <div class="user-data">
-            <label> Usuários Conservadores: {{ $conservador }}%</label><br>
+            <canvas id="chartTypeInvestor" width="400" height="400"></canvas>
 
-            <label> Usuários Moderados: {{ $moderado }}%</label><br>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            <script>
+            // Selecione o contexto do gráfico
+            var ctx = document.getElementById('chartTypeInvestor').getContext('2d');
 
-            <label> Usuários Agressivos: {{ $agressivo }}%</label><br>
+            // Defina os dados e opções
+            var data = {
+                labels: ['Conservadores', 'Moderados', 'Agressivos'],
+                datasets: [{
+                    data: [<?= $conservador ?>, <?= $moderado ?>, <?= $agressivo ?>],
+                    backgroundColor: ['#7dc1ff', '#9e63ff', '#f62e2e'], // Cores para cada setor
+                }]
+            };
+
+            // Defina as opções do gráfico para mostrar a porcentagem no hover
+            var options = {
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                var label = context.label;
+                                label = context.parsed + '%';
+                                return label;
+                            }
+                        }
+                    }
+                }
+            };
+
+            // Crie o gráfico de setores com as opções
+            var myPieChart = new Chart(ctx, {
+                type: 'pie',
+                data: data,
+                options: options
+            });
+            </script>
+
+
+
 
             <label> Total de perfis ativos: {{ $test }}</label><br>
 
-            <label> Proporção de usuárias: {{ $women }}%</label><br>
+            <canvas id="chartUsers" width="400" height="400"></canvas>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-            <label> Proporção de usuários: {{ $men }}%</label><br>
+            <script>
+            // Selecione o contexto do gráfico
+            var ctx = document.getElementById('chartUsers').getContext('2d');
 
-            <label> Proporção de não-identificados: {{ $NI }}%</label><br>
+            // Defina os dados e opções
+            var data = {
+                labels: ['Mulher', 'Homem', 'Preferiu não se identificar'],
+                datasets: [{
+                    data: [<?= $women ?>, <?= $men ?>, <?= $NI ?>],
+                    backgroundColor: [' #fc0fc0', 'blue', 'darkgray'], // Cores para cada setor
+                }]
+            };
+
+            // Defina as opções do gráfico para mostrar a porcentagem no hover
+            var options = {
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                var label = context.label;
+                                label = context.parsed + '%';
+                                return label;
+                            }
+                        }
+                    }
+                }
+            };
+
+            // Crie o gráfico de setores com as opções
+            var myPieChart = new Chart(ctx, {
+                type: 'pie',
+                data: data,
+                options: options
+            });
+            </script>
 
             <label> Número total de usuários: {{ $users }}</label><br>
-
             <label> Usuários que ganham 1 a 3 salários mínimos: {{ $range1 }}%</label><br>
-
             <label> Usuários que ganham 4 a 7 salários mínimos: {{ $range2 }}%</label><br>
-
             <label> Usuários que ganham 8 a 10 salários mínimos: {{ $range3 }}%</label><br>
-
             <label> Usuários que ganham 11 a 15 salários mínimos: {{ $range4 }}%</label><br>
-
             <label> Usuários que ganham mais de 15 salários mínimos: {{ $range5 }}%</label><br>
+
+            <canvas id="chartUserSalary" width="400" height="400"></canvas>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+            <script>
+            // Selecione o contexto do gráfico
+            var ctx = document.getElementById('chartUserSalary').getContext('2d');
+
+            // Defina os dados e opções
+            var data = {
+                labels: ['1 a 3 salários mínimos', '4 a 7 salários mínimos', '8 a 10 salários mínimos',
+                    '8 a 10 salários mínimos', '11 a 15 salários mínimos', 'mais de 15 salários mínimos'
+                ],
+                datasets: [{
+                    data: [<?= $range1 ?>, <?= $range2 ?>, <?= $range3 ?>, <?= $range4 ?>, <?= $range5 ?>],
+                    backgroundColor: [' #fc0fc0', 'blue', 'darkgray'], // Cores para cada setor
+                }]
+            };
+
+            // Defina as opções do gráfico para mostrar a porcentagem no hover
+            var options = {
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                var label = context.label;
+                                label = context.parsed + '%';
+                                return label;
+                            }
+                        }
+                    }
+                }
+            };
+
+            // Crie o gráfico de setores com as opções
+            var myPieChart = new Chart(ctx, {
+                type: 'pie',
+                data: data,
+                options: options
+            });
+            </script>
         </div>
     </div>
 </x-layout.head>
